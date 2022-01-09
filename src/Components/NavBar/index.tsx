96ff73c2fdd55f4d26d6b48e9640501bfe191ee2
devUserContact <developer.user.contact@protonmail.com>
2022-01-09 14:28:10 -0800

    added responsive design to NavBar component

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -4,19 +4,11 @@ import '../../index.scss';
 
 export const NavBar = () => {
 	return (
-	<div className="nav-bar-wrapper">
-		<ul className="nav-bar">		
-				<Link to="/">home </Link>			
-		</ul>
-		<ul className="nav-bar">		
-				<Link to="/about">about </Link>
-		</ul>
-		<ul className="nav-bar">		
-				<Link to="/media">media </Link>
-		</ul>
-		<ul className="nav-bar">		
-				<Link to="/test">test </Link>
-		</ul>
+	<div className="navBar-container">
+		<Link className="navBar" to="/">home </Link>			
+		<Link className="navBar" to="/about">about </Link>
+		<Link className="navBar" to="/media">media </Link>
+		<Link className="navBar" to="/test">test </Link>
 	</div>
 	)	
 }
